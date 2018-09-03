d8a7151af6ad9a8d0a6133466c93d4e8aaff66f2
iamthewit <bencross86@gmail.com>
2018-09-03 22:35:38 +0100

    Creating fresh DB DB everytime phpunit is run

---
+++ phpunit-bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__.'/index.php';
+
+$rootPath = getenv('PROJECT_ROOT_PATH');
+$dbPath = $rootPath.'/resources/sqlite/database/'.getenv('SQLITE_DATABASE_PATH');
+
+// delete existing test DB
+if (file_exists($dbPath)) {
+    unlink($dbPath);
+}
+
+// touch new test DB
+touch($dbPath);
+
+// migrate DB
+$conn = new \TubeHousePrice\Application\DatabaseConnection\SqliteConnection($dbPath);
+$conn->exec(file_get_contents($rootPath.'/resources/sqlite/migrations/migrations.sql'));
--- src/TubeHousePrice/Application/DatabaseConnection/SqliteConnection.php
+++ src/TubeHousePrice/Application/DatabaseConnection/SqliteConnection.php
@@ -6,15 +6,11 @@ use Medoo\Medoo;
 
 final class SqliteConnection extends Medoo
 {
-    private $pathToDatabaseFile;
-
     public function __construct($pathToDatabaseFile)
     {
-        $this->pathToDatabaseFile = $pathToDatabaseFile;
-        
         $options = [
             'database_type' => 'sqlite',
-            'database_file' => $this->pathToDatabaseFile,
+            'database_file' => $pathToDatabaseFile,
         ];
         parent::__construct($options);
     }
